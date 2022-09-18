5ddc21e578e0d63e6d42a9ef07f75da2d1c46978
nijuyonkadesu <24ksriramv@gmail.com>
2022-09-18 12:18:37 +0530

    update subtotal on choosing a entree

--- app/src/main/java/com/njk/smollunchtray/EntreeFragment.kt
+++ app/src/main/java/com/njk/smollunchtray/EntreeFragment.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.njk.smollunchtray.databinding.FragmentEntreeBinding
+import com.njk.smollunchtray.model.LunchViewModel
+
+const val CAULIFLOWER_PRICE = 7.0
+const val BEAN_PRICE = 4.0
+const val PASTA_PRICE = 5.50
+const val SKILLET_PRICE = 5.50
 
 class EntreeFragment: Fragment() {
     private var binding: FragmentEntreeBinding? = null
+    val sharedViewModel: LunchViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +27,25 @@ class EntreeFragment: Fragment() {
         return binding!!.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            entreeFragment = this@EntreeFragment
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    fun chooseEntree(choice: Int){
+        when (choice) {
+            1 -> {sharedViewModel.updateSubtotal(CAULIFLOWER_PRICE)}
+            2 -> {sharedViewModel.updateSubtotal(BEAN_PRICE)}
+            3 -> {sharedViewModel.updateSubtotal(PASTA_PRICE)}
+            4 -> {sharedViewModel.updateSubtotal(SKILLET_PRICE)}
+        }
     }
 }
--- app/src/main/java/com/njk/smollunchtray/model/LunchViewModel.kt
+++ app/src/main/java/com/njk/smollunchtray/model/LunchViewModel.kt
@@ -1,7 +1,31 @@
 package com.njk.smollunchtray.model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.njk.smollunchtray.BEAN_PRICE
+import java.text.NumberFormat
 
 class LunchViewModel: ViewModel() {
-    // TODO: figure out variable needed
+    private var _subtotal = MutableLiveData<Double>()
+    val subtotal: LiveData<String> = Transformations.map(_subtotal){
+        NumberFormat.getCurrencyInstance().format(it)
+    }
+
+    private val _total = MutableLiveData<Double>()
+
+    init {
+        cleanVariables()
+    }
+
+    fun updateSubtotal(increment: Double){
+        if(_total.value == 0.0)
+            _subtotal.value = increment
+    }
+
+    private fun cleanVariables(){
+        _subtotal.value = BEAN_PRICE
+        _total.value = 0.0
+    }
 }
